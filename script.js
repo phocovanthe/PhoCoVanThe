99fa77a76aaf708a1be6cb631e5675fdcda2959d
VuVanNghia <nghiavu2k2abc@gmail.com>
2022-08-17 01:31:44 +0700

    first commit11

---
+++ script.js
@@ -0,0 +1,18 @@
+var modal = document.getElementById('myModal');
+var img = document.getElementById('Tranh1');
+var modalImg = document.getElementById("img01");
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function () {
+    modal.style.display = "none";
+}
+window.onload = () => {
+    let imgs = document.getElementsByClassName("PhongTo");
+    if (imgs.length > 0) {
+        for (let i of imgs) {
+            i.onclick = function () {
+                modal.style.display = "block";
+                modalImg.src = this.src;
+            }
+        }
+    }
+};
